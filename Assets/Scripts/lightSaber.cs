a5a8c1bf40db081738b41630e0e6e3e19f3adfbb
Amirpouya Ghasemaghaei <104846627+ArioAghaei@users.noreply.github.com>
2023-01-27 19:55:56 -0500

    Lightsbaer working

--- Assets/Scripts/lightSaber.cs
+++ Assets/Scripts/lightSaber.cs
@@ -1,40 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class lightSaber : MonoBehaviour
 {
     private GameObject Saber;
     bool saberOn = false;
     private Vector3 saberLength;
-    public OVRInput.Controller controller;
+    public InputActionReference AButton;
     // Start is called before the first frame update
     void Start()
     {
+        // source = gameObject.GetComponent<AudioSource>();
         Saber = transform.Find("SingleLine-LightSaber").gameObject;
         saberLength = Saber.transform.localScale;
         Saber.transform.localScale = new Vector3(saberLength.x, 0, saberLength.z);
+        Saber.SetActive(false);
+
     }
     // Update is called once per frame
     void Update()
     {
+        powerButton();
         lightSaber_Power();
     }
-
+    private void powerButton()
+    {
+        if (AButton.action.WasPressedThisFrame())
+        {
+            saberOn = !saberOn;
+        }
+    }
     private void lightSaber_Power()
     {
-        if (OVRInput.GetDown(OVRInput.Button.One, controller))
+        if (Saber.transform.localScale.y < saberLength.y && saberOn)
         {
-            if (Saber.transform.localScale.y == 0 && saberOn)
-            {
-                Saber.transform.localScale -= new Vector3(0, 0.0001f, 0);
-                saberOn = false;
-            }
-            else
-            {
-                Saber.transform.localScale += new Vector3(0, 0.0001f, 0);
-                saberOn = true;
-            }
+            Saber.transform.localScale += new Vector3(0, 0.01f, 0);
+            Saber.SetActive(true);
+        }
+        else if (Saber.transform.localScale.y > 0 && !saberOn)
+        {
+            Saber.transform.localScale -= new Vector3(0, 0.01f, 0);
+        }
+        if (Saber.transform.localScale.y < 0 || Saber.transform.localScale.y==0)
+        {
+            Saber.SetActive(false);
         }
     }
 }
