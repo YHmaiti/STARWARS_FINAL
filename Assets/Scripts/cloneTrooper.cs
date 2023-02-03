f1db98afcbb19d53bf928943b46ff14d06e09975
Amirpouya Ghasemaghaei <104846627+ArioAghaei@users.noreply.github.com>
2023-02-03 17:18:15 -0500

    Navmesh working

--- Assets/Scripts/cloneTrooper.cs
+++ Assets/Scripts/cloneTrooper.cs
@@ -6,24 +6,34 @@ using UnityEngine.AI;
 public class cloneTrooper : MonoBehaviour
 {
     // Creat enemy and make them shoot at the player
-    public NavMeshAgent enemy;
-    public Transform Player;
+    public NavMeshAgent Enemy;
+    public Transform playerPosition;
     private Animator animator;
+    public GameObject Blaster;
+    public GameObject blasterRay;
+    //public GameObject CloneTrooper;
+    //public GameObject Player;
     // Start is called before the first frame update
     void Start()
     {
+        animator = this.GetComponent<Animator>();
         animator.SetBool("isDead", false);
+        animator.SetBool("isMoving", true);
+        //Enemy = GetComponent<NavMeshAgent>();
+        // rotate blaster to face player
+       // blasterRay.transform.LookAt(playerPosition);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        enemy.SetDestination(Player.position);
-        // if destination is reached then change animation
-        if (enemy.remainingDistance <= enemy.stoppingDistance)
+        transform.LookAt(playerPosition);
+        Enemy.SetDestination(playerPosition.position);
+        // if reached player then stop moving
+        if (Vector3.Distance(transform.position, playerPosition.position) < 10f)
         {
-            animator.SetBool("isDead", true);
+            animator.SetBool("isMoving", false);
         }
-
     }
 }
